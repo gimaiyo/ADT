44744a6c29ff5bc0c963c8912a064e9037563bb0
Rufus Ngugi Mbugua <mbuguarufus@gmail.com>
2013-05-20 07:55:24 +0300

    Initial

---
+++ application/views/template.php
@@ -0,0 +1,265 @@
+<?php
+if (!$this -> session -> userdata('user_id')) {
+	redirect("User_Management/login");
+}
+if (!isset($link)) {
+	$link = null;
+}
+$access_level = $this -> session -> userdata('user_indicator');
+$user_is_administrator = false;
+$user_is_facility_administrator=false;
+$user_is_nascop = false;
+$user_is_pharmacist = false;
+
+if ($access_level == "system_administrator") {
+	$user_is_administrator = true;
+}
+else if ($access_level == "facility_administrator") {
+	$user_is_facility_administrator = true;
+}
+else if ($access_level == "pharmacist") {
+	$user_is_pharmacist = true;
+
+}
+else if ($access_level == "nascop_staff") {
+	$user_is_nascop = true;
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title><?php echo $title;?></title>
+<link rel="SHORTCUT ICON" href="<?php echo base_url().'Images/favicon.ico'?>">
+<link href="<?php echo base_url().'CSS/style.css'?>" type="text/css" rel="stylesheet"/> 
+<link href="<?php echo base_url().'CSS/jquery-ui.css'?>" type="text/css" rel="stylesheet"/>
+
+<link href="<?php echo base_url().'CSS/datatable/jquery.dataTables.css'?>" type="text/css" rel="stylesheet"/>
+<link href="<?php echo base_url().'CSS/datatable/jquery.dataTables_themeroller.css'?>" type="text/css" rel="stylesheet"/>
+<link href="<?php echo base_url().'CSS/datatable/demo_table.css" type="text/css'?>" rel="stylesheet"/>
+
+<script src="<?php echo base_url().'Scripts/jquery.js'?>" type="text/javascript"></script> 
+<script src="<?php echo base_url().'Scripts/jquery-ui.js'?>" type="text/javascript"></script> 
+<script src="<?php echo base_url().'Scripts/jquery.form.js'?>" type="text/javascript"></script>
+<script src="<?php echo base_url().'Scripts/jquery.blockUI.js'?>" type="text/javascript"></script>
+<!-- Datatables -->
+<script type="text/javascript" src="<?php echo base_url().'Scripts/datatable/jquery.dataTables.min.js'?>"></script>
+<!-- Datatables end --> 
+
+
+<!--Load datatables settings -->
+<script type="text/javascript">
+	$(document).ready(function() {
+	    oTable = $('.setting_table').dataTable({
+	    	"sScrollY": "240px",
+	        "bJQueryUI": true,
+	        "sPaginationType": "full_numbers"
+	    });
+	} );
+
+</script>
+
+<?php
+if ($user_is_pharmacist) {
+	echo "<script src=\"" . base_url() . "Scripts/offline_database.js\" type=\"text/javascript\"></script>";
+}
+if (isset($script_urls)) {
+	foreach ($script_urls as $script_url) {
+		echo "<script src=\"" . $script_url . "\" type=\"text/javascript\"></script>";
+	}
+}
+?>
+
+<?php
+if (isset($scripts)) {
+	foreach ($scripts as $script) {
+		echo "<script src=\"" . base_url() . "Scripts/" . $script . "\" type=\"text/javascript\"></script>";
+	}
+}
+?>
+
+
+ 
+<?php
+if (isset($styles)) {
+	foreach ($styles as $style) {
+		echo "<link href=\"" . base_url() . "CSS/" . $style . "\" type=\"text/css\" rel=\"stylesheet\"/>";
+	}
+}
+?>  
+<?php
+if (!$user_is_pharmacist) {?>
+<script type="text/javascript">
+	$(document).ready(function() {
+		$("#my_profile_link_container").hover(function() {
+			var html = "<a href='<?php echo site_url('user_management/change_password')?>' class='top_menu_link temp_link'>Change Password</a> <a href='<?php echo site_url('user_management/logout');?>' class='top_menu_link temp_link'>Logout</a> ";
+			$("#my_profile_link").css('display','none'); 
+			$(this).append(html);
+		}, function() {
+			$("#my_profile_link").css('display','block');
+			$(this).find(".temp_link").remove();
+		});
+	});
+
+</script>
+<?php }?>
+<style>
+	#my_profile_link_container .generated_link{
+		display: none;
+	}
+	#my_profile_link{ 
+		width: 200px !important;
+		margin:0px !important;
+		padding:0px !important;
+	}
+	#my_profile_link_continer{
+		min-width: 200px !important;
+		background-color: red;
+		height:100px;
+	}
+	.temp_link{
+		font-size: 10px;
+		width:100px !important;
+		background-color: #B80000;  
+		margin:0px;
+	}
+	.dataTables_wrapper{
+		width: 80%;
+		margin:0 auto;
+	}
+
+
+	table.setting_table{
+		border:solid;
+		border-color: grey;
+		border-width: 1px;
+	}
+	.setting_table td{
+
+		max-width: 300px;
+	}
+
+	.ui-widget-header{
+		background:rgb(140, 214, 140);
+	}
+	table.dataTable tr.odd{
+		background-color:rgb(234,255,232);
+	}
+	table.dataTable tr.odd td sorting_1{
+		background-color:rgb(234,255,232);
+	}
+
+</style>
+
+
+</head>
+
+<body>
+<div id="wrapper">
+	<div id="top-panel" style="margin:0px;">
+
+		<div class="logo">
+			<a class="logo" href="<?php echo base_url();?>" ></a> 
+</div>
+<?php if ($user_is_pharmacist) {?>
+	<div id="synchronize">
+		<div id="loadingDiv"></div>
+		<div id="dataDiv" style="display: none;">
+		<span style="display: block; font-size: 12px; margin: 10px 5px;">Number of Local Patients: <span id="total_number_local"></span></span>
+		<span style="display: block; font-size: 12px; margin: 10px 5px;">Number of Patients Registered: <span id="total_number_registered"></span></span>
+		</div>
+		<a class="action_button" id="synchronize_button" href="<?php echo base_url();?>synchronize_pharmacy">Synchronize Now</a>
+	</div>
+	<?php }?>
+
+				<div id="system_title">
+					<span style="display: block; font-weight: bold; font-size: 24px; margin:2px;">Ministry of Health</span>
+					<span style="display: block; font-size: 20px;">ARV Drugs Supply Chain Management Tool</span>
+					<?php
+					if ($user_is_pharmacist) {?>
+						<style>
+							#facility_name {
+								color: green;
+								margin-top: 5px;
+								font-weight: bold;
+							}
+							#synchronize_button{
+								display: none;
+								width: 200px;
+								margin: 0;
+								height: 40px;
+								position: absolute;
+								top:3.5px;
+								left:30px;		
+								line-height: 40px;
+														
+							}
+							
+						</style>
+						<div id="facility_name">
+							
+							<span style="display: block; font-size: 14px;"><?php echo $this -> session -> userdata('facility_name');?></span>
+						</div>
+					<?php }?>
+				</div>
+				<div class="banner_text" style="font-size: 22px;"><?php echo $banner_text;?></div>
+ <div id="top_menu"> 
+
+ 	<?php
+	//Code to loop through all the menus available to this user!
+	//Fet the current domain
+	$menus = $this -> session -> userdata('menu_items');
+	$current = $this -> router -> class;
+	$counter = 0;
+	if($menus){
+?>
+ 	<a href="<?php  echo site_url('home_controller');?>" class="top_menu_link  first_link <?php
+	if ($current == "home_controller") {echo " top_menu_active ";
+	}
+?>">Home </a><?php }?>
+<?php
+if($menus){
+foreach($menus as $menu){?>
+	<a href = "<?php echo site_url($menu['url']);?>" class="top_menu_link <?php
+	if ($current == $menu['url'] || $menu['url'] == $link) {echo " top_menu_active ";
+	}
+?>"><?php echo $menu['text']; if($menu['offline'] == "1"){?>
+	 <span class="alert red_alert">off</span></a>
+	
+<?php } else{?>
+	 <span class="alert green_alert">on</span></a>
+<?php }?>
+
+
+
+<?php
+$counter++;
+}}
+if($menus){
+	?>
+<div id="my_profile_link_container" style="display: inline">
+<a ref="#" class="top_menu_link" id="my_profile_link"><?php echo $this -> session -> userdata('full_name');?></a>
+</div>
+<?php }?>
+ </div>
+
+</div>
+
+<div id="inner_wrapper"> 
+
+
+<div id="main_wrapper"> 
+ 
+<?php $this -> load -> view($content_view);?>
+ 
+ 
+ 
+<!-- end inner wrapper --></div>
+  <!--End Wrapper div--></div>
+    <div id="bottom_ribbon" style="top:200px; width:90%;">
+        <div id="footer">
+ <?php $this -> load -> view("footer_v");?>
+    </div>
+    </div>
+</body>
+</html>
---
+++ application/views/transferdestination_v.php
@@ -0,0 +1,186 @@
+<style type="text/css">
+	.actions_panel {
+		width: 200px;
+		margin-top: 5px;
+	}
+	.hovered td {
+		background-color: #E5E5E5 !important;
+	}
+	a{
+		text-decoration: none;
+	}
+	.enable_user{
+		color:green;
+		font-weight:bold;
+	}
+	.disable_user{
+		color:red;
+		font-weight:bold;
+	}
+	.edit_user{
+		color:blue;
+		font-weight:bold;
+	}
+	.passmessage {
+
+		display: none;
+		background: #00CC33;
+		color: black;
+		text-align: center;
+		height: 20px;
+		padding:5px;
+		font: bold 1px;
+		border-radius: 8px;
+		width: 30%;
+		margin-left: 30%;
+		margin-right: 10%;
+		font-size: 16px;
+		font-weight: bold;
+	}
+	.errormessage {
+
+		display: none;
+		background: #FF0000;
+		color: black;
+		text-align: center;
+		height: 20px;
+		padding:5px;
+		font: bold 1px;
+		border-radius: 8px;
+		width: 30%;
+		margin-left: 30%;
+		margin-right: 10%;
+		font-size: 16px;
+		font-weight: bold;
+	}
+	#DataTables_Table_0_wrapper{
+		width: 40%;
+	}
+
+
+</style>
+<script type="text/javascript">
+	$(document).ready(function() {
+		//This loop goes through each table row in the page and applies the necessary modifications
+		$.each($(".table_row"), function(i, v) {
+			//First get the row id which will be used later
+			var row_id = $(this).attr("row_id");
+			//This gets the first td element of that row which will be used to add the action links
+			var first_td = $(this).find("td:first");
+			//Get the width of this td element in integer form (i.e. remove the .px part)
+			var width = first_td.css("width").replace("px", "");
+			//If the width is less than 200px, extend it to 200px so as to have a more uniform look
+			if(width < 200) {
+				first_td.css("width", "200px");
+			} 
+			/*
+			//Append the contents of the 'action_panel_parent' to this first td element
+			$($("#action_panel_parent").html()).appendTo(first_td);
+			//Loop through all the links included in the action panel for this td and append the row_id to the end of it
+			$.each($(this).find(".link"), function(i,v){
+				var current_link = $(this).attr("link");
+				var new_link = $(this).attr("link")+row_id; 
+				$(this).attr("href",new_link);
+			});
+			*/
+		});
+		//Add a hover listener to all rows
+		$(".table_row").hover(function() {
+			//When hovered on, make the background color of the row darker and show the action links
+			$(this).addClass("hovered");
+			$(this).find(".actions_panel").css("visibility", "visible");
+		}, function() {
+			//When hovered off, reset the background color and hide the action links
+			$(this).removeClass("hovered");
+			$(this).find(".actions_panel").css("visibility", "hidden");
+		});
+		
+		//When clicked dialog form for new indication pops up
+		$("#new_client").click(function(){ 
+			$("#client_form").dialog("open");
+		});
+		
+		//Dialog form for new user form
+		$("#client_form").dialog({
+			height : 200,
+			width : 300,
+			modal : true,
+			autoOpen : false
+		});
+		
+		//count to check which message to display
+        var count='<?php echo @$this -> session -> userdata['message_counter']?>';
+        var message='<?php echo @$this -> session -> userdata['message']?>';	
+	
+	if(count == 1) {
+	$(".passmessage").slideDown('slow', function() {
+
+	});
+	$(".passmessage").append(message);
+
+	var fade_out = function() {
+	$(".passmessage").fadeOut().empty();
+	}
+	setTimeout(fade_out, 5000);
+     <?php 
+     $this -> session -> set_userdata('message_counter', "0");
+     $this -> session -> set_userdata('message', " ");
+     ?>
+
+	}
+	if(count == 2) {
+	$(".errormessage").slideDown('slow', function() {
+
+	});
+	$(".errormessage").append(message);
+
+	var fade_out = function() {
+	$(".errormessage").fadeOut().empty();
+	}
+	setTimeout(fade_out, 5000);
+     <?php 
+     $this -> session -> set_userdata('message_counter', "0");
+     $this -> session -> set_userdata('message', " ");
+     ?>
+
+	}
+		
+	});
+
+</script>
+<div id="action_panel_parent" style="display:none">
+	<div class="actions_panel" style="visibility:hidden" >
+		<?php
+//Loop through all the actions passed on to this file
+foreach($actions as $action){
+		?>
+		<a class="link" link="<?php echo $this->router->class."/".$action[1]."/"?>"><?php echo $action[0]
+		?></a>
+		<?php }?>
+	</div>
+</div>
+
+<a class="action_button" id="new_client">New Transfer To</a>
+<div class="passmessage"></div>
+<div class="errormessage"></div>
+<?php
+echo $sources;
+?>
+<div id="client_form" title="New Transfer Destination">
+	<?php
+	$attributes = array('class' => 'input_form');
+	echo form_open('transferdestination_management/save', $attributes);
+	echo validation_errors('<p class="error">', '</p>');
+	?>
+		
+	<label>
+<strong class="label">Transfer Destination Name</strong>
+<input type="text" name="source_name" id="source_name" class="input" size="30">
+</label>
+
+
+	<label>
+
+	<input type="submit" value="Save" class="submit-button"/>
+	</form>
+</div>
