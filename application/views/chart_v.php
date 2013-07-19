5573f14b7b2f38f4a34a10e3ab5a9114189c1e02
Rufus Ngugi Mbugua <mbuguarufus@gmail.com>
2013-07-19 10:35:39 +0300

    Paused Minification Plugin to fix BUGS

--- application/views/chart_v.php
+++ application/views/chart_v.php
@@ -1,5 +1,4 @@
 <?php
-$this->load->view('sections/head');
 $chartSize=0;
 if($resultArraySize<=6){
 	$chartSize='300';
@@ -19,7 +18,7 @@ if($resultArraySize>20){
 if($resultArraySize>25){
 	$chartSize='2100';
 }
-echo $chartSize;
+
 ?>
 
 <script>
