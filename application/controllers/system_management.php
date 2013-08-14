4565be78c210bd1868a34837447cd2245751ec09
Rufus Ngugi Mbugua <mbuguarufus@gmail.com>
2013-08-14 08:41:37 +0300

    Carabiner now fixed and in effect

--- application/controllers/system_management.php
+++ application/controllers/system_management.php
@@ -18,12 +18,20 @@ class System_management extends MY_Controller {
 		$this -> load -> library('carabiner');
 		$this -> carabiner -> empty_cache();
 		// add a js file
-		$jsArray = array(array('jquery-1.10.2.min.js'), array('highcharts/highcharts.js'), array('highcharts/modules/exporting.js')
-		,array('jquery.form.js'),array('bootstrap/bootstrap.min.js')
-		,array('datatable/jquery.dataTables.js'),array('jquery-migrate-1.2.1.js')
-		, array('jquery.multiselect.js'),array('Merged_JS.js'));
-		$cssArray = array( array('style.css'), array('bootstrap.css'),array('jquery-ui.css'),array('style_report.css'),array('jquery.dataTables_themeroller.css')
-		,array('demo_table.css'));
+				$jsArray = array(array('jquery-1.10.2.js'),array('jquery-migrate-1.2.1.js'),array('jquery.form.js'),
+		 array('jquery-ui.js'),array('sorttable.js'),array('datatable/jquery.dataTables.min.js'),
+		 array('bootstrap/bootstrap.min.js'),array('bootstrap/paging.js'),array('Merged_JS.js'),
+		 array('highcharts/highcharts.js'),array('highcharts/modules/exporting.js'));
+
+		
+		$cssArray = array(array('bootstrap.css'),
+		array('bootstrap-responsive.min.css'),array('datatable/jquery.dataTables.css'),
+		array('datatable/jquery.dataTables_themeroller.css'),
+		array('datatable/demo_table.css'),array('style.css')
+		,array('jquery-ui.css'),array('style_report.css'),array('jquery.dataTables_themeroller.css')
+		,array('demo_table.css'),array('jquery.multiselect.css'),array('jquery.multiselect.filter.css'),
+		array('prettify.css'),array('style_report.css'),array('validator.css')	
+		);
 
 		
 
