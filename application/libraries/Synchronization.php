13c5c7b3660a161de3c5156a15e52e48807d139f
KevinMarete55443 <kevomarete@gmail.com>
2013-08-19 11:31:18 +0300

    Sync

--- application/libraries/Synchronization.php
+++ application/libraries/Synchronization.php
@@ -5,22 +5,24 @@ class Synchronization {
 	}
 
 	public function upload_connect($url, $data) {
-		extract($_POST);
-		$url = $url;
-		$fields = $data;
-		$fields_string = '';
-		foreach ($fields as $key => $value) { $fields_string .= $key . '=' . $value . '&';
-		}
-		rtrim($fields_string, '&');
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_POST, count($fields));
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
-		$headers = array('Content-type: application/json');
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		$result = curl_exec($ch);
-		curl_close($ch);
-		return $result;
+		 extract($_POST);
+		 $url = $url;
+		 $fields = $data;
+		 $fields_string = '';
+		 foreach ($fields as $key => $value) { $fields_string .= $key . '=' . $value . '&';
+		 }
+		 rtrim($fields_string, '&');
+		 $ch = curl_init();
+		 curl_setopt($ch, CURLOPT_URL, $url);
+		 curl_setopt($ch, CURLOPT_POST, count($fields));
+		 curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+		 $headers = array('Content-type: application/json');
+		 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		 $result = curl_exec($ch);
+		 curl_close($ch);
+		 return $result;
+
 	}
+
 }
 ?>
